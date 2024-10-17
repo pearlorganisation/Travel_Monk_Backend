9900a82c54a4d86dba4bcbf10f9acbd68d82d7c1
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-10-17 10:22:38 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ app.use("/api/v1/packages", packageRouter);
 app.use("/api/v1/trips", tripsRouter);
 app.use("/api/v1/activities", activityRouter);
 app.use("/api/v1/contact", contactRouter);
-app.use("/api/v1/destination", destinationRouter);
+app.use("/api/v1/destinations", destinationRouter);
 
 app.use(notFound);
 app.use(errorHandler);
