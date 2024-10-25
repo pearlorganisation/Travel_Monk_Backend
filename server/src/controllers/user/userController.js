19a01b5dff4ff68852b7934ce761deac796a7acb
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-10-25 18:44:29 +0530

    minot fix

--- server/src/controllers/user/userController.js
+++ server/src/controllers/user/userController.js
@@ -153,7 +153,7 @@ export const getUserDetails = asyncHandler(async (req, res, next) => {
     return next(new ApiErrorResponse("User is not found", 404));
   }
   return res
-    .status()
+    .status(200)
     .json({ success: true, message: "User found successfully", data: user });
 });
 
