564d60b9c62a23b5d6f13e486a0acdd1ccc0de44
Ranjan <ranjan@pearlorganisation.com>
2024-10-25 14:47:19 +0530

    minor

--- server/src/models/busCruiseContact/busCruiseContact.js
+++ server/src/models/busCruiseContact/busCruiseContact.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const busCruiseContactSchema = new mongoose.Schema(
   {
-    fullName: {
+    name: {
       type: String,
       required: true,
       trim: true,
