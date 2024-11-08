3e619851ce8e9b0a4229e3b427e15969a025b13a
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-11-08 12:32:23 +0530

    create booking done

--- server/src/controllers/booking/bookingController.js
+++ server/src/controllers/booking/bookingController.js
@@ -1,30 +1,49 @@
 import { asyncHandler } from "../../utils/errors/asyncHandler.js";
 import { razorpayInstance } from "../../configs/razorpay/razorpay.js";
 import crypto from "crypto";
+import PreBuiltPackageBooking from "../../models/booking/preBuiltPackageBooking.js";
 
 export const createBooking = asyncHandler(async (req, res, next) => {
-  const { amount, currency } = req.body;
+  const { totalPrice, user, packageId, numberOfTravellers } = req.body;
 
   // Basic validation
-  if (!amount || !currency) {
+  if (!totalPrice) {
     return res
       .status(400)
       .json({ message: "Amount and currency are required" });
   }
   const options = {
-    amount: amount * 100, // Convert amount to smallest unit (paise for INR)
-    currency: currency,
+    amount: totalPrice * 100, // Convert amount to smallest unit (paise for INR)
+    currency: "INR",
     receipt: `order_rcptid_${Math.floor(1000 + Math.random() * 9000)}`, // Generate unique receipt id
   };
 
   // Create the order using Razorpay instance
-  const order = await razorpayInstance.orders.create(options);
+  try {
+    const order = await razorpayInstance.orders.create(options);
+    const preBuiltPackageBooking = await PreBuiltPackageBooking.create({
+      bookingId: `BID_${Date.now()}`,
+      user: "670f7d22de6dad830bd241e0",
+      packageId: "670e521c1a925010284229bf",
+      numberOfTravellers: 3,
+      totalPrice,
+      bookingStatus: "Pending",
+      paymentStatus: "Unpaid",
+      razorpay_order_id: order.id,
+    });
 
-  // Return success response with order details
-  res.status(200).json({
-    success: true,
-    order,
-  });
+    // Return success response with order details
+    res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.error("Error creating Razorpay order:", error); // Log the complete error object for debugging
+    return res.status(500).json({
+      message: "Failed to create Razorpay order",
+      error: error.message,
+    });
+  }
 });
 
 // Verify payment signature
@@ -38,6 +57,10 @@ export const verifyPayment = asyncHandler(async (req, res, next) => {
     .digest("hex");
 
   if (generated_signature === razorpay_signature) {
+    await PreBuiltPackageBooking.findOneAndUpdate(
+      { razorpay_order_id },
+      { paymentStatus: "Paid", bookingStatus: "Completed" }
+    );
     res
       .status(200)
       .json({ success: true, message: "Payment verified successfully" });
--- server/src/models/booking/preBuiltPackageBooking.js
+++ server/src/models/booking/preBuiltPackageBooking.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
 // PreBuiltPackageBooking Schema
-const preBuiltPackageBookingSchema = new Schema(
+const preBuiltPackageBookingSchema = new mongoose.Schema(
   {
     bookingId: { type: String, required: true, unique: true },
-    user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    packageId: { type: Schema.Types.ObjectId, ref: "Package", required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    packageId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Package",
+      required: true,
+    },
     numberOfTravellers: { type: Number, required: true },
     totalPrice: { type: Number, required: true },
     customizations: [
@@ -36,6 +40,12 @@ const preBuiltPackageBookingSchema = new Schema(
       enum: ["Paid", "Unpaid"],
       default: "Unpaid",
       required: true,
+    },
+    razorpay_order_id: {
+      type: String,
+    },
+    razorpay_payment_id: {
+      type: String,
     },
   },
   { timestamps: true }
