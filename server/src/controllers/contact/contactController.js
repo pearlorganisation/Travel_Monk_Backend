027187539ed0c6064522102edf54231699967aa2
Ranjan <ranjan@pearlorganisation.com>
2024-11-20 15:03:36 +0530

    fixed contact form submit fail

--- server/src/controllers/contact/contactController.js
+++ server/src/controllers/contact/contactController.js
@@ -5,22 +5,7 @@ import { paginate } from "../../utils/pagination.js";
 
 export const submitContactForm = asyncHandler(async (req, res, next) => {
     // validation 
-   const {name , email, phoneNumber, message } = req.body;
-   // try catch for validation
-   try {
-       const validateData = contactSchemaValidation.parse({name , email, phoneNumber, message});
-
-       console.log(validateData);
-   } catch (error) {
-      if (error instanceof z.ZodError) {
-
-        console.error(error.errors);
-        return next(new ApiErrorResponse("Enter correct no format",error))  
-      } else {
-        console.error("An unexpected error occurred:", error);
-        return next(new ApiErrorResponse("Unexpected error",error));
-      }
-   }
+  const {name , email, phoneNumber, message } = req.body;
   const contactInfo = await Contact.create(req.body);
   if (!contactInfo) {
     return next(new ApiErrorResponse("Contact form submission failed", 400));
