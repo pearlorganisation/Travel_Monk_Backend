967b7af120523e1b3394bb5626141eb70431eb7e
nayan-kunwar26 <nayan@pearlorganisation.com>
2024-11-25 18:04:15 +0530

    create vehicle api

--- server/src/models/Vehicle/Vehicle.js
+++ server/src/models/Vehicle/Vehicle.js
@@ -2,36 +2,33 @@ import mongoose from "mongoose";
 
 const vehicleSchema = new mongoose.Schema(
   {
-    // vehicleName: {
-    //   type: String,
-    //   // required: true,
-    //   unique: true,
-    // },
-    // passengerCapacity: {
-    //   type: Number,
-    //   // required: true,
-    // },
-    // luggageCapacity: {
-    //   type: Number,
-    //   // required: true,
-    // },
-    // price: {
-    //   type: Number,
-    //   // required: true,
-    // },
-    // images: [
-    //   {
-    //     secure_url: { type: String },
-    //     public_id: { type: String },
-    //   },
-    // ],
-    amenities: [
-      { name: String, icon: { secure_url: String, public_id: String } },
+    vehicleName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passengerCapacity: {
+      type: Number,
+      required: true,
+    },
+    luggageCapacity: {
+      type: Number,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    images: [
+      {
+        secure_url: { type: String, required: true },
+        public_id: { type: String, required: true },
+      },
     ],
-    // isAvailable: {
-    //   type: Boolean,
-    //   default: true, // Vehicles are available by default
-    // },
+    isAvailable: {
+      type: Boolean,
+      default: true, // Vehicles are available by default
+    },
   },
 
   { timestamps: true }
