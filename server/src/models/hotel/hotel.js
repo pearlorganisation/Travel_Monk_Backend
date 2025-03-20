399de546098869bfe43afbd5922c54f99779c5e6
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-03-20 09:59:33 +0530

    feat: add trim option to hotel schema fields for improved data consistency

--- server/src/models/hotel/hotel.js
+++ server/src/models/hotel/hotel.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const hotelSchema = new mongoose.Schema(
   {
-    name: { type: String, unique: true, required: true },
-    slug: { type: String, unique: true, required: true },
+    name: { type: String, unique: true, required: true, trim: true },
+    slug: { type: String, unique: true, required: true, trim: true },
     destination: { type: mongoose.Schema.Types.ObjectId, ref: "Destination" },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    country: { type: String, required: true },
+    city: { type: String, required: true, trim: true },
+    state: { type: String, required: true, trim: true },
+    country: { type: String, required: true, trim: true },
     estimatedPrice: { type: Number, required: true },
     amenities: [String], // Chips or multiselect
     inclusion: [String],
