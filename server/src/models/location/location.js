d22e264cb86bd43736f32e10c016e5ffd3b2cb95
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-03-31 14:35:12 +0530

    feat: refactor location schema to improve coordinate validation and structure

--- server/src/models/location/location.js
+++ server/src/models/location/location.js
@@ -1,18 +1,35 @@
 import mongoose from "mongoose";
 
-const locationsSchema = new mongoose.Schema({
-  day: {
-    type: Number,
+const geoSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true,
   },
-  location: [
-    {
-      name: String,
-      coordinates: {
-        type: { type: String, enum: ["Point"], required: true },
-        coordinates: { type: [Number], required: true },
-      },
+  coordinates: {
+    type: [Number],
+    required: true,
+    validate: {
+      validator: ([lng, lat]) =>
+        Array.isArray([lng, lat]) &&
+        lat >= -90 &&
+        lat <= 90 &&
+        lng >= -180 &&
+        lng <= 180,
+      message:
+        "Invalid coordinates: Latitude must be between -90 and 90, Longitude must be between -180 and 180.",
     },
-  ],
+  },
+});
+
+const locationSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  coordinates: { type: geoSchema, required: true },
+});
+
+const locationsSchema = new mongoose.Schema({
+  day: { type: Number, required: true },
+  location: [locationSchema],
   destination: { type: mongoose.Schema.Types.ObjectId, ref: "Destination" },
 });
 
