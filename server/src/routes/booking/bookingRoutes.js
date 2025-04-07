da0020f6c22f5b71780afccc9dcb9bc9a0882483
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-04-07 16:21:49 +0530

    comment added

--- server/src/routes/booking/bookingRoutes.js
+++ server/src/routes/booking/bookingRoutes.js
@@ -24,10 +24,13 @@ const router = express.Router();
 router.route("/").post(authenticateToken, createBooking);
 router.route("/verify-payment").post(authenticateToken, verifyPayment);
 router.route("/me").get(authenticateToken, myBookings);
+
+//GET bookings of PRE BUILT PACKAGE ENQUIRY done by user and also get the all booking for admin panel
 router
   .route("/pre-built-package-enquiry")
   .get(authenticateToken, getAllPreBuiltPackageEnquiryBookings); // works for both user and admin
 
+//GET bookings of FULLY CUSTOMIZE PACKAGE ENQUIRY done by user and also get the all booking for admin panel
 router
   .route("/fully-customize-package-enquiry")
   .get(authenticateToken, getAllFullyCustomizeEnquiryBookings);
@@ -38,19 +41,23 @@ router.route("/").get(
   getAllBookings // Pre-built package bookings
 ); //For Admin
 
+// VERIFY PAYMENT FOR PRE BUILT PACKAGE ENQUIRY BOOKING
 router
   .route("/pre-built-package-enquiry/verify-payment")
   .post(authenticateToken, verifyPreBuiltPackageEnquiryPayment);
 
+// VERIFY PAYMENT FOR FULLY CUSTOMIZE PACKAGE ENQUIRY BOOKING
 router
   .route("/fully-customize-package-enquiry/verify-payment")
   .post(authenticateToken, verifyFullyCustomizeEnquiryPayment);
 
+// CREATE ORDER FOR PRE BUILT PACKAGE ENQUIRY
 router
   .route("/pre-built-package-enquiry/:id") // id of pre-built package enquiry
   .post(authenticateToken, createPreBuiltPackageEnquiryBooking); // Create new enquiry
 // .patch(updatePreBuiltPackageEnquiryBooking); // Update an existing enquiry
 
+// CREATE ORDER FOR FULLY CUSTOMIZE PACKAGE ENQUIRY
 router
   .route("/fully-customize-package-enquiry/:id")
   .post(authenticateToken, createFullyCustomizeEnquiryBooking);
