0e70ea815e8c893bf2baca32f96667367bf06916
nayan-kunwar26 <nayan@pearlorganisation.com>
2025-04-09 13:53:28 +0530

    feat: import Destinations model in vehicle controller

--- server/src/controllers/vehicle/vehicleController.js
+++ server/src/controllers/vehicle/vehicleController.js
@@ -6,6 +6,7 @@ import path from "path";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import { deleteFile } from "../../utils/fileUtils.js";
+import Destinations from "../../models/destination/destinations.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
